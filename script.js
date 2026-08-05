68fa0df57bef3b761e7cbae76ea28cdd6ecb1311
SkepticleOrc <cjonesy9@googlemail.com>
2026-08-05 19:17:00 +0100

    Final Chrome Viewport Fix
    Chrome browser was skewing page container past the desired viewport scale of 1. Fix implemented site wide to address the issue. Rigorous testing all afternoon brought me to this fix...
    
    `<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">`

--- script.js
+++ script.js
@@ -23,28 +23,3 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navi.classList.toggle("active");
 });
-
-// Prevents accidental multi-touch pinch zooming from breaking the layout
-//document.addEventListener(
-  //"touchstart",
-  //function (event) {
-   // if (event.touches.length > 1) {
-     // event.preventDefault();
-   // }
- // },
-//  { passive: false },
-//);
-
-// Prevents double-tap zooming which can also trigger layout skewing
-//let lastTouchEnd = 0;
-//document.addEventListener(
-  //"touchend",
-  //function (event) {
-    //const now = new Date().getTime();
-    //if (now - lastTouchEnd <= 300) {
-      //event.preventDefault();
-    //}
-    //lastTouchEnd = now;
-  //},
-  //{ passive: false },
-//);
